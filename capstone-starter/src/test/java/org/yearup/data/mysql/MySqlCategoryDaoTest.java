f811269ca92b2a0bae0ffac8ff5df0ee3cbec843
aGonzalezAppDev <agonzalez@appdev.yearup.org>
2023-06-30 07:59:35 -0400

    Added getByIdTest for Categories

--- capstone-starter/src/test/java/org/yearup/data/mysql/MySqlCategoryDaoTest.java
+++ capstone-starter/src/test/java/org/yearup/data/mysql/MySqlCategoryDaoTest.java
@@ -1,8 +1,42 @@
 package org.yearup.data.mysql;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.yearup.models.Category;
+
 import static org.junit.jupiter.api.Assertions.*;
 
-class MySqlCategoryDaoTest {
+class MySqlCategoryDaoTest extends BaseDaoTestClass {
     // add Category Unit Tests
+
+    private MySqlCategoryDao dao;
+
+    @BeforeEach
+    public void setup() {
+        dao = new MySqlCategoryDao(dataSource);
+    }
+
+    @Test
+    public void getById_shouldReturn_RightCategory(){
+        // arrange
+        int category_id = 1;
+        Category expected = new Category()
+        {{
+            setCategoryId(1);
+            setName("Electronics");
+            setDescription("Explore the latest gadgets and electronic devices.");
+
+        }};
+
+        // act
+        var actual = dao.getById(category_id);
+
+        //assert
+        assertEquals(expected.getName(), actual.getName(), "Because I tried to get the first category from the database.");
+
+    }
+
+
+
 
 }
